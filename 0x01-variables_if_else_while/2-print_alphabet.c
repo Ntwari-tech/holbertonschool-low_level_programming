ae47be40fc37079df8a58abcbbf2ca85cd545527
Ntwari-tech <ntwari.yannick@gmail.com>
2021-06-27 17:09:27 -0700

    lowcase alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - entry point
+*
+*function name - putchar
+*
+*Description - Void = no value
+*
+*Return: 0 - Success
+*
+*/
+char  main(void)
+{
+char c = 'a';
+for (char c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+
+}
