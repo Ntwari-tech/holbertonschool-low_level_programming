0a69c5e0e4def85b42c5306f7a5c8b73e0958ef2
Ntwari-tech <ntwari.yannick@gmail.com>
2021-06-27 20:09:54 -0700

    single dig putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - entry point
+*
+*function name - putchar
+*
+*Description - Void = no value
+*
+*Return: 0 - Success
+*
+*/
+int main(void)
+{
+char ot = '0';
+
+for (ot = '0'; ot <= '9'; ot++)
+
+putchar(ot);
+
+putchar('\n');
+
+return (0);
+}
