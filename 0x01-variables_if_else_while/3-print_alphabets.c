4431dc3b165aec92a5b98e5c1579d0199ee09949
Ntwari-tech <ntwari.yannick@gmail.com>
2021-06-27 17:57:13 -0700

    lowcase and upcase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - entry point
+*
+*function name - putchar
+*
+*Description - Void = no value
+*
+*Return: 0 - Success
+*/
+
+char main(void)
+{
+char c = 'a';
+for (char c = 'a'; c <= 'z'; c++)
+putchar(c);
+for (c = 'A';  c <= 'Z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+
+}
