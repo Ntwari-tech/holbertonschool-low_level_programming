41af4e540e67fdf482e6a0243f9b4fc4042b7c19
Ntwari-tech <ntwari.yannick@gmail.com>
2021-06-27 20:36:56 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - entry point
+*
+*function name - putchar
+*
+*Description - Void = no value
+*
+*Return: 0 - Success
+*
+*/
+int main(void)
+{
+
+int y;
+for (y = 'z'; y >= 'a'; y--)
+
+putchar(y);
+
+putchar('\n');
+
+return (0);
+
+
+}
