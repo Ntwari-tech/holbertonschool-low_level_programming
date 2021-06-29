dc5e8fa7ec7cc8a0a81c78b27277c695464d3b4c
Ntwari-tech <ntwari.yannick@gmail.com>
2021-06-28 18:49:39 -0700

    0-holberton,c

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+**main - entry point
+*
+*function name - _putchar
+*
+*Description - Void = no value
+*
+*Return: 0 - Success
+*
+*/
+
+int main(void)
+
+{
+
+char *s = "Holberton\n";
+
+while (*s)
+
+_putchar(*s++);
+
+return (0);
+}
+
