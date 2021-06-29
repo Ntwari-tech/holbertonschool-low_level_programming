c56d0247a3d2e02b141e93b95332e772cc546467
Ntwari-tech <ntwari.yannick@gmail.com>
2021-06-28 19:45:42 -0700

    l alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+*print_alphabet - entry point
+*
+*function name - _putchar
+*
+*Description - Void = no value
+*
+*Return: 0 - Success
+*
+*/
+
+void print_alphabet(void)
+{
+char c = 'a';
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+
+}
