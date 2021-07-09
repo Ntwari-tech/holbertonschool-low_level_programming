d128b31852dae49d62b227e1d18831b152aa7b3d
Ntwari-tech <ntwari.yannick@gmail.com>
2021-07-09 05:49:21 -0700

    int x *= int y

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * mul - Function
+ * Description: Multiply two numbers
+ * Return: x *= y
+ * @x: first int
+ * @y: second int
+ */
+int mul(int x, int y)
+{
+
+	return (x * y);
+}
