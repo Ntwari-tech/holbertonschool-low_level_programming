fbd35fd52a93e66799420908d3fa9ef534c914f6
Ntwari-tech <ntwari.yannick@gmail.com>
2021-07-13 10:19:20 -0700

    update value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - F() name
+ *
+ * Description: f() that takes *P to an in and updates the () to 98
+ *
+ * @n: our pointer
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
