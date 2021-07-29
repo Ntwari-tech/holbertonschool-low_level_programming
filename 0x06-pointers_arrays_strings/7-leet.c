ca5c997022e68102601ad6273f7d6106a0efa6e0
Ntwari-tech <ntwari.yannick@gmail.com>
2021-07-28 20:46:34 -0700

    encodes the strings into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * leet- f() name
+ * @str: pointer to string
+ *
+ * Return: str
+ */
+
+char *leet(char *str)
+{
+	char *characters;
+	char *numbers;
+	int x;
+	int y;
+
+	characters = "aAeEoOtTlL";
+	numbers = "4433007711";
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		for (y = 0; characters[y] != '\0'; y++)
+		{
+			if (str[x] == characters[y])
+				str[x] = numbers[y];
+		}
+	}
+	return (str);
+}
