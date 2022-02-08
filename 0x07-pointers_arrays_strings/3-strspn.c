ab20d9363478a78e0149a28e25a846cb901920ac
ntwari-tech <ntwari.yannik@gmail.com>
2022-02-08 12:30:22 -0800

    length of prefix substrng

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,29 +2,28 @@
 #include <stdio.h>
 
 /**
- * _strspn - main function
- * @accept: string
- * @s: variable
- * Return: x
- */
+* _strspn - main function
+* @accept: string
+* @s: variable
+* Return: x
+*/
 unsigned int _strspn(char *s, char *accept)
-                                                                     
-{                                                                                                                
-        int x, y;                                                                                                
-        for (x = 0; s[x] != '\0'; x++)                                                                           
-        {                                                                                                        
-                for (y = 0; accept[x] != '\n'; y++)                                                              
-                {                                                                                                
-                        if (accept[y] == s[x])                                                                   
-                        {                                                                                        
-                                break;                                                                           
-                        }                                                                                        
-                        if (accept[y] == '\0')                                                                   
-                        {                                                                                        
-                                return (x);                                                                      
-                        }                                                                                        
-                }                                                                                                
-        }                                                                                                        
-        return (0);                                                                                              
+
+{
+int x, y;
+for (x = 0; s[x] != '\0'; x++)
+{
+for (y = 0; accept[x] != '\n'; y++)
+{
+if (accept[y] == s[x])
+{
+break
+}
+if (accept[y] == '\0')
+{
+return (x);
+}
 }
 
+return (0);
+}
