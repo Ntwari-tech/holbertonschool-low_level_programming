e1b9963d43ccdaf042ccb4efdd3f2ff81353b9b2
ntwari-tech <ntwari.yannik@gmail.com>
2022-02-09 17:46:37 -0800

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int _sqrt(int x, int y);
+/**
+* _sqrt - main fucntion
+* @y: is the root
+* @x: is the base number
+* Return: returns the square root
+*/
+
+int _sqrt(int x, int y)
+{
+	int sqrt = y * y;
+
+	if (sqrt > x)
+	return (-1);
+
+	if (sqrt == x)
+	return (y);
+
+	return (_sqrt(x, y + 1));
+}
+/**
+* _sqrt_recursion - calc  square root
+* @n: is an int
+* Return: is void
+*/
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 0));
+}
