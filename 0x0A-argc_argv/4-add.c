9c589b119ca3026b7c3995623214c61e8558f66b
ntwari-tech <ntwari.yannik@gmail.com>
2022-02-15 14:34:08 -0800

    + positve #

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - is our function
+* @argc:  contains the number of arg
+* @argv: an array of strings
+*
+*Return: void
+*/
+int main(int argc, char *argv[])
+{
+	int i, digit;
+	int sum = 0;
+
+for (i = 1; i < argc; i++)
+{
+for (digit = 0; argv[i][digit]; digit++)
+{
+if (argv[i][digit] > '0' && argv[i][digit] < '9')
+if (argv[i][digit] < '0' || argv[i][digit] > '9')
+{
+printf("Error\n");
+return (1);
+}
+}
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
+return (0);
+}
