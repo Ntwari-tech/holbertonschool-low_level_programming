f24ebd9f96708a29a44c3d896017f3e7b7fa0990
ntwari-tech <ntwari.yannik@gmail.com>
2022-02-15 15:10:11 -0800

    + positve #

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,7 +17,7 @@ for (i = 1; i < argc; i++)
 {
 for (digit = 0; argv[i][digit]; digit++)
 {
-if (argv[i][digit] > '0' && argv[i][digit] < '9')
+/** if (argv[i][digit] > '0' && argv[i][digit] < '9')*/
 if (argv[i][digit] < '0' || argv[i][digit] > '9')
 {
 printf("Error\n");
