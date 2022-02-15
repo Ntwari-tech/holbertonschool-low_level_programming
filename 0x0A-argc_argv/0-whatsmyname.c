f701f40a96693554c9f4c6f3e4233e115fba1d4d
ntwari-tech <ntwari.yannik@gmail.com>
2022-02-15 12:16:09 -0800

    prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - is our function
+* @argc:  contains the number of arg
+* @argv: an array of strings
+*
+*Return: an integer
+*/
+int main(__attribute__((unused))int argc, char *argv[])
+
+{
+
+printf("%s\n", argv[0]);
+return (0);
+
+}
