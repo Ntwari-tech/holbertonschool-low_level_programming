c7b8b5e7d8711e17b88984a94546474ab7154474
ntwari-tech <ntwari.yannik@gmail.com>
2022-02-21 19:15:37 -0800

    concat two strng

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,43 +1,43 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
+#include <string.h>
 /**
-* string_nconcat - concatenates two strings
-* @s1: 1st Strng
-* @s2: 2nd Strng
-* @n: variable
-* Return: a *p to a string
-*/
-
+ * string_nconcat - concatenates two strings up to n chars
+ * @n: where s2 is cut
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ *
+ * Return: pointer to catted string
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, x, l = 0;
-	char *cat;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	while (s1[i])
-		i++;
-
-	     l = i + n;
-
-	cat = malloc((sizeof(char) * l) + 1);
-	if (cat == NULL)
-		return (NULL);
-
-	x = 0;
-
-	for (i = 0; s1[i]; i++)
-
-		cat[x++] = s1[i];
-
-	for (i = 0; s2[i] && i < n; i++)
-		cat[x++] = s2[i];
-cat[x] = '\0';
-
-return (cat);
-
+char *s;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+if (n >= strlen(s2))
+{
+s = malloc((strlen(s1) + strlen(s2)) + 1);
+if (s == NULL)
+{
+return (NULL);
+}
+s = strcpy(s, s1);
+strcpy((s + strlen(s1)), s2);
+}
+else
+{
+s = malloc((strlen(s1) + n) + 1);
+if (s == NULL)
+{
+return (NULL);
+}
+s = strcpy(s, s1);
+strncat(s, s2, n);
+}
+return (s);
 }
