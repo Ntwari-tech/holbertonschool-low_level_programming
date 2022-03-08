bf355082addbfe858a57b39929505008c4033bc4
Ntwari-tech <ntwari.yannick@gmail.com>
2022-03-08 10:59:56 -0800

    prt all elments

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements
+ * @h: pointer
+ * return: the number of nodes
+ *
+ */
+
+size_t print_list(const list_t *h)
+
+{
+int find;
+if (h == NULL)
+return (0);
+
+for (find = 0; h != NULL; h = h->next, find++)
+{
+if (h->str == NULL)
+printf("[%i] %s\n", 0, "(nil)");
+else
+printf("%i %s\n", h->len, h->str);
+}
+return (find);
+}
