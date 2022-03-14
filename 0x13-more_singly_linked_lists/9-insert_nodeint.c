ebbf0c7a9f4524cfa0c5513a51a854d04aa94a97
Ntwari-tech <ntwari.yannick@gmail.com>
2022-03-14 14:37:15 -0700

    inserts new node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *ty = *head;
 	unsigned int len = 0;
 
-	if (*head == NULL)
+	if (*head == NULL || idx < 0)
 		return (NULL);
 
 	nx = malloc(sizeof(listint_t));
