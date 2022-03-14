4a299536c93589d332e2fde4325a787fae14b0a1
Ntwari-tech <ntwari.yannick@gmail.com>
2022-03-14 14:42:04 -0700

    inserts

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *ty = *head;
 	unsigned int len = 0;
 
-	if (*head == NULL || idx < 0)
+	if (*head == NULL)
 		return (NULL);
 
 	nx = malloc(sizeof(listint_t));
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 	nx->next = ty;
 	*head = ty;
-	return (*head);
+	return (nx);
 	}
 
 	while (len < idx - 1)
