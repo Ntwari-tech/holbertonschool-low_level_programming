1a421989a3fbd742f6a452ce49392d368cf84c88
Ntwari-tech <ntwari.yannick@gmail.com>
2022-03-14 14:49:35 -0700

    insert4

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	while (len < idx - 1)
 	{
-		if (ty->next == NULL)
+		if (ty->next == NULL || ty == NULL)
 			return (NULL);
 		ty = ty->next;
 		len++;
